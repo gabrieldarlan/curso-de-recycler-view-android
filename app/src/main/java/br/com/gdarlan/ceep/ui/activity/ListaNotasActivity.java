3b979fb5a32e9bf23c292ac127d6d49ceddfc829
gabrieldarlan <gabriel.darlan88@gmail.com>
2021-01-17 22:04:09 -0300

    cap04-03 Adicionando o menu no formulário

--- app/src/main/java/br/com/gdarlan/ceep/ui/activity/ListaNotasActivity.java
+++ app/src/main/java/br/com/gdarlan/ceep/ui/activity/ListaNotasActivity.java
@@ -1,6 +1,9 @@
 package br.com.gdarlan.ceep.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -26,6 +29,15 @@ public class ListaNotasActivity extends AppCompatActivity {
         final List<Nota> todasNotas = notasDeExemplo();
         configuraRecyclerView(todasNotas);
 
+        TextView botaoInsereNota = findViewById(R.id.lista_notas_insere_nota);
+        botaoInsereNota.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Intent iniciaFormularioNota = new Intent(ListaNotasActivity.this,
+                        FormularioNotaActivity.class);
+                startActivity(iniciaFormularioNota);
+            }
+        });
     }
 
     private List<Nota> notasDeExemplo() {
